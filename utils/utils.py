83a26ce16f5f12295489b0f345b36505050caa91
CamaroTheBOSS <plessico@wp.pl>
2023-10-23 19:00:40 +0200

    Improved image saving function. Now it is saving mosaic with superresolution done by model, high resolution ground truth and bilinear interpolation done by deterministic algorithm. Everything done on unpadded images

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,10 @@
 import os
-import uuid
 
 import cv2
 import numpy as np
+import torch
 import yaml
+from torch import Tensor
 
 
 def init_training_directory(output_root: str, run_name: str) -> str:
@@ -23,10 +24,29 @@ def save_results(img_dict, output_dir):
         os.makedirs(output_dir)
 
     i = 0
-    for batch_list in img_dict["SR"]:
-        for frame in batch_list:
-            img = frame.detach().permute(1, 2, 0).cpu().numpy() * 255.
-            img = img.astype(np.uint8)
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-            cv2.imwrite(f"{output_dir}/superresolution-{(4 - len(str(i))) * '0' + str(i)}.png", img)
+    for b_prediction, b_label, b_bilinear in zip(img_dict["SR"], img_dict["HR"], img_dict["BILINEAR"]):
+        for prediction, label, bilinear in zip(b_prediction, b_label, b_bilinear):
+            x1, y1, x2, y2 = get_size_without_zero_padding(label, padding_value=-1)
+            prediction = prediction[:, y1:y2, x1:x2]
+            label = label[:, y1:y2, x1:x2]
+            bilinear = bilinear[:, y1:y2, x1:x2]
+
+            upper_row = torch.cat((prediction, label), dim=2)
+            lower_row = torch.cat((bilinear, label), dim=2)
+            output = torch.cat((upper_row, lower_row), dim=1)
+
+            output = np.clip((output.detach().permute(1, 2, 0).cpu().numpy() + 1) * 127.5, 0, 255)
+            output = output.astype(np.uint8)
+            output = cv2.cvtColor(output, cv2.COLOR_RGB2BGR)
+            cv2.imwrite(f"{output_dir}/superresolution-{(4 - len(str(i))) * '0' + str(i)}.png", output)
             i += 1
+
+
+def get_size_without_zero_padding(image: Tensor, padding_value=0):
+    dummy = torch.argwhere(image != padding_value)
+    max_x = dummy[:, 2].max()
+    min_x = dummy[:, 2].min()
+    min_y = dummy[:, 1].min()
+    max_y = dummy[:, 1].max()
+
+    return min_x, min_y, max_x, max_y
