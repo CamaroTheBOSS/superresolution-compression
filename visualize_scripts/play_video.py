9b7aceed635309dd2f845a6eabcf9f0ce6927114
CamaroTheBOSS <plessico@wp.pl>
2023-10-24 20:43:53 +0200

    Added docstring to play_video function

--- visualize_scripts/play_video.py
+++ visualize_scripts/play_video.py
@@ -36,6 +36,24 @@ def augmentation(video: Tensor):
 
 @torch.no_grad()
 def play_video(path_to_txt_file: str, video_transform: callable = lambda x: (x, x), fps: int = 30, repeat=True):
+    """
+    Function, which transform video from specified path with given callback function and plays it in cv2 window.
+
+    Note that callback function must take video Tensor with shape N, C, H, W where N is number of frames in the video
+    and should return tuple with original video Tensor at the first place and transformed video Tensor at the second
+    place. Both Tensors should have the same shape N, C, H, W to ensure that this function will visualize well.
+
+    It's possible to restart the visualization. It is needed to click 'Q' at the keyboard. If so, transform will be
+    repeated at the next start of the video visualization.
+
+    :param path_to_txt_file: string path to text file with paths to video frame images
+    :param video_transform: function which takes video Tensor with shape N, C, H, W where N is number of frames in the
+    video and returns tuple with original video Tensor at the first place and transformed video Tensor at the second
+    place. Both Tensors should have the same shape N, C, H, W to ensure that this function will visualize well.
+    :param fps: Speed of visualization
+    :param repeat: If true, visualization will be repeated after finishing
+    :return:
+    """
     with open(path_to_txt_file) as f:
         root = os.path.dirname(path_to_txt_file)
         transforms = get_transforms()
